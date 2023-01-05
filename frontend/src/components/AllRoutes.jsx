ec09fff5a0a90d335101d7739110e16fbf6547a7
Ankur982 <singh999ankur@gmail.com>
2023-01-05 05:42:44 +0530

    updaiting imgae upload functionality

--- frontend/src/components/AllRoutes.jsx
+++ frontend/src/components/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/Home";
+import { PublishBlog } from "../pages/publishBlog/PublishBlog";
 
 
 
@@ -9,6 +10,7 @@ const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/addBlog" element={<PublishBlog />} />
     
     </Routes>
   );
--- frontend/src/pages/publishBlog/PublishBlog.jsx
+++ frontend/src/pages/publishBlog/PublishBlog.jsx
@@ -1,24 +1,78 @@
-import React from 'react'
-import { Box, Image, Text } from '@chakra-ui/react'
-import home from "../../utils/home.svg"
+import React from "react";
+import { Box, Image, Input, Text, Textarea } from "@chakra-ui/react";
+import home from "../../utils/home.svg";
+import post from "../../utils/post.svg";
 export const PublishBlog = () => {
+  let [title, setTitle] = React.useState("");
+  let [summary, setSummary] = React.useState("");
+  let [image, setImage] = React.useState("");
+console.log("fasdbf",image)
   return (
-    <Box>
-      <Box>
-
+    <Box display={"flex"} w={"90%"} m={"auto"} mt={"30px"}>
+      <Box display={"flex"} flexDirection={"column"} gap={"30px"} position={"absolute"} top={"30%"}>
+        <Box
+          width={"80px"}
+          height={"70px"}
+          background={"#E9E9E9"}
+          borderRadius={"20px"}
+          m={"auto"}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Image w={"80px"} h={"52px"} src={home} />
+        </Box>
+        <Box
+          width={"80px"}
+          height={"70px"}
+          background={"#E9E9E9"}
+          borderRadius={"20px"}
+          m={"auto"}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Image w={"70px"} h={"52px"} src={post} />
+        </Box>
       </Box>
-      <Box
-        width={"80px"}
-        height={"70px"}
-        background={"#E9E9E9"}
-        borderRadius={"20px"}
-        m={"auto"}
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
-        <Image w={"40px"} h={"52px"} src={home} />
+      <Box w={"78.7%"} border={"1px solid"} m={"auto"}>
+        <Box
+          m={"auto"}
+          width={"919px"}
+          height={"443px"}
+          fontFamily={"Inter"}
+          fontStyle={"normal"}
+          fontWeight={"300"}
+          textAlign={"justify"}
+          color={"#767676"}
+          mt={"20px"}
+        >
+          <Input
+            value={title}
+            placeholder="Enter Title"
+            fontWeight={"600"}
+            fontSize={"40px"}
+            lineHeight={"48px"}
+            mt={"20px"}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <Input type="file" multiple={false} mt={"20px"}  onChange={({ e }) => setImage( e)}/>
+          <Textarea
+            fontWeight={"300"}
+            fontSize={"40px"}
+            lineHeight={"48px"}
+            value={summary}
+            name={"file"}
+            onChange={(e) => {
+              setSummary(e.target.value);
+            }}
+            mt={"20px"}
+            placeholder="Ener content"
+          />
+        </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
